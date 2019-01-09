838f0960551f11ec47981c9bdc401d5a61355016
Maria Lopez Pozuelo <mlpozuelo@google.com>
2019-01-09 18:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ const ageIsValid = Number.isInteger(currentAge)
 const randomNumber = (Math.random() * 20) + 1;
 
 const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1000000000
